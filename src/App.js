f6a9650efbf8ec691a190bed0f60e6816985776e
Yyyurii <yurii.kryvko@gmail.com>
2022-04-24 00:08:09 +0300

    Секція Header з адаптацією

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import logo from './assets/img/logo.svg';
 import instagram from './assets/img/instagram.svg';
 import facebook from './assets/img/facebook.svg';
 
-import { Button, Row, Col, Container, Nav } from 'react-bootstrap';
+import { Button, Row, Col, Container, Nav, Navbar } from 'react-bootstrap';
 
 
 function App() {
@@ -20,27 +20,25 @@ function App() {
                 <img className="logo-img" src={logo} alt="logo" />
               </Col>
               <Col>
-                <Nav
-                  activeKey="/home"
-                  onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-                  className="justify-content-end"
-                >
-                  <Nav.Item>
-                    <Nav.Link className="link-light" href="/home" eventKey="Inventory">Inventory</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link className="link-light" eventKey="Financing">Financing</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link className="link-light" eventKey="Contacts">Contacts</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link className="link-light" eventKey="instagram"><img src={instagram} alt="instagram" /></Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link className="link-light" eventKey="facebook"><img src={facebook} alt="facebook" /></Nav.Link>
-                  </Nav.Item>
-                </Nav>
+
+
+                <Navbar expand="lg" variant="light" className="navbar-dark navbar-white">
+                  <Container className="justify-content-end" variant="navbar-light"> 
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" className="justify-content-end"/>
+                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                      <Nav className="justify-content-end">
+                        <Nav.Link className="navLink" href="/home" eventKey="Inventory">Inventory</Nav.Link>
+                        <Nav.Link className="navLink" eventKey="Financing">Financing</Nav.Link>
+                        <Nav.Link className="navLink" eventKey="Contacts">Contacts</Nav.Link>
+                        <Nav.Link className="navLink" eventKey="instagram"><img src={instagram} alt="instagram" /></Nav.Link>
+                        <Nav.Link className="navLink" eventKey="facebook"><img src={facebook} alt="facebook" /></Nav.Link>
+                      </Nav>
+                    </Navbar.Collapse>
+                  </Container>
+                </Navbar>
+
+
+
               </Col>
             </Row>
           </Container>
@@ -54,18 +52,18 @@ function App() {
               </Col>
             </Row>
             <Row>
-              <Col className="col-3 ">
+              <Col className="col-xl-3 col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <p className="text-light">Monza Motorsports is a boutique-style dealership specializing in premium imports. We offer an advanced online vehicle purchasing experience   with integrated financing and contactless home delivery.</p>
               </Col>
-              <Col className="col-3 ">
+              <Col className="col-xl-3 col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <p className="text-light">All of our cars come with a 5 day/500 kilometres no-hassle exchange option. Since 2012 Monza Motorsports
                   has been the benchmark for innovation in the automotive industry.</p>
               </Col>
             </Row>
             <Row>
-                <Button className="btn btn-outline-light btn-lg btn-headerInfo col-3">
-                  Browse cars
-                </Button>
+              <Button className="btn btn-outline-light btn-lg btn-headerInfo col-xs-6 col-sm-8 col-md-8 col-lg-6 col-xl-3">
+                Browse cars
+              </Button>
             </Row>
           </Container>
         </section>
