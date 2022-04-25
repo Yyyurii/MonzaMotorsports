7f9f7e0af231b65320f4e50338b7ec3b8cb78074
Yyyurii <yurii.kryvko@gmail.com>
2022-04-25 23:31:01 +0300

    Верстка секції Products

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.scss';
 import logo from './assets/img/logo.svg';
 import instagram from './assets/img/instagram.svg';
 import facebook from './assets/img/facebook.svg';
+import sortBy from './assets/img/sortBy.svg';
+import vwTiguan from './assets/img/cars/vw-tiguan.svg';
 
-import { Button, Row, Col, Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Row, Col, Container, Nav, Navbar, Form, Card, CardGroup } from 'react-bootstrap';
 
 
 function App() {
@@ -23,8 +25,8 @@ function App() {
 
 
                 <Navbar expand="lg" variant="light" className="navbar-dark navbar-white">
-                  <Container className="justify-content-end" variant="navbar-light"> 
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" className="justify-content-end"/>
+                  <Container className="justify-content-end" variant="navbar-light">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" className="justify-content-end" />
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                       <Nav className="justify-content-end">
                         <Nav.Link className="navLink" href="/home" eventKey="Inventory">Inventory</Nav.Link>
@@ -61,13 +63,162 @@ function App() {
               </Col>
             </Row>
             <Row>
-              <Button className="btn btn-outline-light btn-lg btn-headerInfo col-xs-6 col-sm-8 col-md-8 col-lg-6 col-xl-3">
+              <Button className="btn btn-outline-light btn-lg btn-headerInfo btn-generalStyles col-xs-6 col-sm-8 col-md-8 col-lg-6 col-xl-3">
                 Browse cars
               </Button>
             </Row>
           </Container>
         </section>
       </section>
+
+      <Container>
+        <Row>
+          <h1 className="title-currentOffers text-center">CURRENT OFFERS</h1>
+        </Row>
+      </Container>
+
+      <Container className="container-formSelect">
+        <Row className="formSelect-row">
+          <Col className="form-select-col">
+            <Form.Select aria-label="Default select example" className="formSelect" data-size="5">
+              <option>Year</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </Col>
+          <Col className="form-select-col">
+            <Form.Select aria-label="Default select example" className="formSelect">
+              <option>Make</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </Col>
+          <Col className="form-select-col">
+            <Form.Select aria-label="Default select example" className="formSelect">
+              <option>Model</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </Col>
+          <Col className="form-select-col">
+            <Form.Select aria-label="Default select example" className="formSelect">
+              <option>Trim</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </Col>
+          <Col className="form-select-col">
+            <Form.Select aria-label="Default select example" className="formSelect">
+              <option>Mileage</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </Col>
+          <Col className="form-select-col col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+            <Button className="btn-generalStyles btn-selectForm" variant="danger">40 cars</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Check
+              label="Instantly available vehicles"
+              className="formCheck" />
+          </Col>
+          <Col className="sort justify-content-end">
+            <div className="sort__label">Sort by<img src={sortBy} alt="sort by" /></div>
+            <Form.Select className="sort__formSelect">
+              <option>Publication date (ascending)</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container className="products">
+
+          <Card className="card">
+            <Card.Img variant="top" src={vwTiguan} className="card__img" />
+            <Card.Body>
+              <Card.Title className="card__title"><span className="card__subtitle">2019</span> Volkswagen Tiguan</Card.Title>
+              <Card.Text className="card__params">19,855 km | Automatic | Diesel</Card.Text>
+              <Card.Text className="card__price">$34,888</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="card">
+            <div className="card__header">
+              <Card.Img variant="top" src={vwTiguan} className="card__img" />
+              <div className="card__label_new">New</div>
+            </div>
+            <Card.Body>
+              <Card.Title className="card__title"><span className="card__subtitle">2019</span> Volkswagen Tiguan</Card.Title>
+              <Card.Text className="card__params">19,855 km | Automatic | Diesel</Card.Text>
+              <Card.Text className="card__price">$34,888</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="card">
+            <div className="card__header">
+              <Card.Img variant="top" src={vwTiguan} className="card__img" />
+              <div className="card__label_sold">SOLD</div>
+            </div>
+            <Card.Body>
+              <Card.Title className="card__title"><span className="card__subtitle">2019</span> Volkswagen Tiguan</Card.Title>
+              <Card.Text className="card__params">19,855 km | Automatic | Diesel</Card.Text>
+              <Card.Text className="card__price">$34,888</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="card">
+            <div className="card__header">
+              <Card.Img variant="top" src={vwTiguan} className="card__img" />
+              <div className="card__label_sold">SOLD</div>
+            </div>
+            <Card.Body>
+              <Card.Title className="card__title"><span className="card__subtitle">2019</span> Volkswagen Tiguan</Card.Title>
+              <Card.Text className="card__params">19,855 km | Automatic | Diesel</Card.Text>
+              <Card.Text className="card__price">$34,888</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="card">
+            <div className="card__header">
+              <Card.Img variant="top" src={vwTiguan} className="card__img" />
+              <div className="card__label_sold">SOLD</div>
+            </div>
+            <Card.Body>
+              <Card.Title className="card__title"><span className="card__subtitle">2019</span> Volkswagen Tiguan</Card.Title>
+              <Card.Text className="card__params">19,855 km | Automatic | Diesel</Card.Text>
+              <Card.Text className="card__price">$34,888</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="card">
+            <div className="card__header">
+              <Card.Img variant="top" src={vwTiguan} className="card__img" />
+              <div className="card__label_sold">SOLD</div>
+            </div>
+            <Card.Body>
+              <Card.Title className="card__title"><span className="card__subtitle">2019</span> Volkswagen Tiguan</Card.Title>
+              <Card.Text className="card__params">19,855 km | Automatic | Diesel</Card.Text>
+              <Card.Text className="card__price">$34,888</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="card">
+            <div className="card__header">
+              <Card.Img variant="top" src={vwTiguan} className="card__img" />
+              <div className="card__label_sold">SOLD</div>
+            </div>
+            <Card.Body>
+              <Card.Title className="card__title"><span className="card__subtitle">2019</span> Volkswagen Tiguan</Card.Title>
+              <Card.Text className="card__params">19,855 km | Automatic | Diesel</Card.Text>
+              <Card.Text className="card__price">$34,888</Card.Text>
+            </Card.Body>
+          </Card>
+
+      </Container>
 
     </div>
   );
