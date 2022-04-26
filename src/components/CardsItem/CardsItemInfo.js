27c2552c5d9326439c89672bb626b554f5e0348a
Yyyurii <yurii.kryvko@gmail.com>
2022-04-26 23:40:31 +0300

    Компонент CardsItemInfo

---
+++ src/components/CardsItem/CardsItemInfo.js
@@ -0,0 +1,103 @@
+const CardsItemInfo = [
+  {
+    id: 1,
+    img: '../../assets/img/cars/vw-tiguan.svg',
+    year: 2019,
+    make: 'volkswagen',
+    model: 'tiguan',
+    trim: null,
+    mileage: '19,855',
+    price: '34,888',
+    label: null
+  },
+  {
+    id: 2,
+    img: '../../assets/img/cars/vw-tiguan.svg',
+    year: 2014,
+    make: 'toyota',
+    model: 'highlander',
+    trim: 'XLE',
+    mileage: '69,000',
+    price: '27,875',
+    label: 'new'
+  },
+  {
+    id: 3,
+    img: '../../assets/img/cars/vw-tiguan.svg',
+    year: 2017,
+    make: 'lexus',
+    model: 'rx 350',
+    trim: 'LUXURY',
+    mileage: '39,850',
+    price: '38,775',
+    label: null
+  },
+  {
+    id: 4,
+    img: '../../assets/img/cars/vw-tiguan.svg',
+    year: 2016,
+    make: 'toyota',
+    model: 'highlander',
+    trim: 'LE',
+    mileage: '104,000',
+    price: '23,728',
+    label: null
+  },
+  {
+    id: 5,
+    img: '../../assets/img/cars/vw-tiguan.svg',
+    year: 2017,
+    make: 'hyundai',
+    model: 'elantra',
+    trim: null,
+    mileage: '95,000',
+    price: '11,788',
+    label: null
+  },
+  {
+    id: 6,
+    img: '../../assets/img/cars/vw-tiguan.svg',
+    year: 2016,
+    make: 'mercedes-benz',
+    model: 'gle',
+    trim: null,
+    mileage: '59,800',
+    price: '38,878',
+    label: null
+  },
+  {
+    id: 7,
+    img: '../../assets/img/cars/vw-tiguan.svg',
+    year: 2017,
+    make: 'toyota',
+    model: 'venza',
+    trim: 'AWD',
+    mileage: '99,000',
+    price: '18,878',
+    label: 'sold'
+  },
+  {
+    id: 8,
+    img: '../../assets/img/cars/vw-tiguan.svg',
+    year: 2016,
+    make: 'lexus',
+    model: 'rx 350',
+    trim: 'F-SPORT',
+    mileage: '86,000',
+    price: '34,878',
+    label: 'new'
+  },
+  {
+    id: 9,
+    img: '../../assets/img/cars/vw-tiguan.svg',
+    year: 2015,
+    make: 'toyota',
+    model: 'venza',
+    trim: 'XLE',
+    mileage: '99,000',
+    price: '18,620',
+    label: null
+  },
+];
+
+export default CardsItemInfo;
